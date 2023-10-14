add335a3d71f772e2e167dac7a3c46ab4b8dca16
sepxx <mihartthx@gmail.com>
2023-10-14 14:28:10 +0300

    test

---
+++ src/main/java/com/bnk/commonrateservicev2/commonRatesProvider/controllers/CommonRateController.java
@@ -0,0 +1,42 @@
+package com.bnk.commonrateservicev2.commonRatesProvider.controllers;
+
+import com.bnk.commonrateservicev2.commonRatesProvider.dto.firstService.InfoToGetRates;
+import com.bnk.commonrateservicev2.commonRatesProvider.services.TimePathService;
+import com.bnk.commonrateservicev2.rater.services.FormCommonRateService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class CommonRateController {
+
+    private final TimePathService timePathService;
+    private final FormCommonRateService formCommonRateService;
+
+//    @GetMapping("/optimal")
+//    public ResponseEntity<?> setPoints(@RequestBody Coordinates coordinates) throws JsonProcessingException {
+//        return ResponseEntity.ok(rateService.getRatesByRoads(coordinates));
+//    }
+
+
+    @GetMapping("/offices/optimal")
+    public ResponseEntity<?> getCommonRates(@RequestBody InfoToGetRates infoToGetRates){
+        Map<Long, Double> commonRatesMap;
+        try {
+            Map<Long, Long> idsToTime = timePathService.getRatesByRoads(infoToGetRates);
+            commonRatesMap = formCommonRateService.getCommonRatesMap(idsToTime, infoToGetRates.getServiceType());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(commonRatesMap);
+    }
+}
---
+++ src/main/java/com/bnk/commonrateservicev2/commonRatesProvider/services/TimePathService.java
@@ -0,0 +1,42 @@
+package com.bnk.commonrateservicev2.commonRatesProvider.services;
+
+import com.bnk.commonrateservicev2.commonRatesProvider.dto.firstService.Container;
+
+import com.bnk.commonrateservicev2.commonRatesProvider.dto.firstService.InfoToGetRates;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class TimePathService {
+
+    private Map<Long, Long> times = new HashMap<>();
+    private final Parser parser;
+
+    public Map<Long, Long> getRatesByRoads(InfoToGetRates infoToGetRates) throws JsonProcessingException {
+        for (int i = 0; i < infoToGetRates.getId().size(); i++){
+            Container container = parser.request(
+                    infoToGetRates.getCoords().get(i).getLat(),
+                    infoToGetRates.getCoords().get(i).getLng(),
+                    infoToGetRates.getUserGeo().getLat(),
+                    infoToGetRates.getUserGeo().getLng(),
+                    infoToGetRates.getId().get(i),
+                    infoToGetRates.getMoveType()
+            );
+            times.put(Long.valueOf(infoToGetRates.getId().get(i)),
+                    Long.valueOf(container.getRows().get(0)
+                            .getElements().get(0)
+                            .getDuration().getValue())
+            );
+        }
+       return times;
+    }
+
+
+
+}
